8f5b77f57ae4ed666b3b3b0db94e37d302af12d6
Tommaso Cucinotta <tommaso.cucinotta@santannapisa.it>
2017-02-27 10:32:27 +0100

    Introduced parameters for FORWARD command: host, port, pkt size.

--- src/message.h
+++ src/message.h
@@ -7,11 +7,17 @@
 typedef enum { COMPUTE, STORE, FORWARD } command_type_t;
 
 typedef struct {
+  in_addr_t fwd_host;	// target IP of host to forward to
+  uint16_t fwd_port;	// target port (for multiple nodes on same host)
+  uint32_t pkt_size;	// size of forwarded packet
+} fwd_opts_t;
+
+typedef struct {
   command_type_t cmd;
   union {
     uint16_t comp_time_us;	// COMPUTE time (usecs)
     uint16_t store_flags;	// STORE flags
-    in_addr_t fwd_host;		// FORWARD host
+    fwd_opts_t fwd;		// FORWARD host+port and pkt size
   } u;
 } command_t;
 
@@ -19,7 +25,7 @@ typedef struct {
   uint32_t req_id;
   uint32_t req_size;	// Overall message size in bytes, including commands and payload
   uint8_t num;		// Number of valid entries in cmds[]
-  command_t cmds[64];	// Up to 64 command_t
+  command_t cmds[];	// Up to 255 command_t
 } message_t;
 
 #endif
