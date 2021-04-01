ea7abc93896bfe10ce472abc32c1f47948a5e8a5
Remo <andreoliremo@gmail.com>
2021-04-01 06:17:26 +0200

    First per-client threading prototype. It should be data race/deadlock/race-condition free. Kind of ugly, node.c should require some reworks for a better implementation. Also, termination is handled more graciously than before.

--- src/node.c
+++ src/node.c
@@ -24,6 +24,8 @@
 #include <pthread.h>
 #include <sys/epoll.h>
 
+#define MAX_EVENTS 10
+
 typedef enum { RECEIVING, SENDING, LOADING, STORING, CONNECTING } req_status;
 
 typedef struct {
@@ -31,16 +33,30 @@ typedef struct {
   unsigned long buf_size;
   unsigned char *curr_buf;
   unsigned long curr_size;
+
+  unsigned char *reply_buf;
+  unsigned char *fwd_buf;
+
+  unsigned char *store_buf;
+
   int sock;
   req_status status;
   int orig_sock_id;             // ID in socks[]
 } buf_info;
 
-static volatile int running = 1; //epoll_main_loop flag
+typedef struct {
+  int epollfd;
+  struct epoll_event events[MAX_EVENTS];
+} thread_info;
+
+
+static volatile int node_running = 1; //epoll_main_loop flag
 
 #define MAX_BUFFERS 16
 
 buf_info bufs[MAX_BUFFERS];
+thread_info thread_infos[MAX_BUFFERS];
+pthread_mutex_t bufs_mtx[MAX_BUFFERS]; //FIXME: Expensive with lots of buf_infos
 
 typedef struct {
   in_addr_t inaddr;	// target IP
@@ -50,6 +66,7 @@ typedef struct {
 
 #define MAX_SOCKETS 16
 sock_info socks[MAX_SOCKETS];
+pthread_mutex_t socks_mtx;
 
 char *bind_name = "0.0.0.0";
 int bind_port = 7891;
@@ -62,22 +79,32 @@ int epollfd;
 
 // return sock associated to inaddr:port
 int sock_find_addr(in_addr_t inaddr, int port) {
+  pthread_mutex_lock(&socks_mtx);
+
   for (int i = 0; i < MAX_SOCKETS; i++) {
     if (socks[i].inaddr == inaddr && socks[i].port == port) {
+      pthread_mutex_unlock(&socks_mtx);
       return socks[i].sock;
     }
   }
+
+  pthread_mutex_unlock(&socks_mtx);
   return -1;
 }
 
 // return index of sock in socks[]
 int sock_find_sock(int sock) {
   assert(sock != -1);
+
+  pthread_mutex_lock(&socks_mtx);
   for (int i = 0; i < MAX_SOCKETS; i++) {
     if (socks[i].sock == sock) {
+      pthread_mutex_unlock(&socks_mtx);
       return i;
     }
   }
+
+  pthread_mutex_unlock(&socks_mtx);
   return -1;
 }
 
@@ -85,44 +112,56 @@ char* storage_path = NULL;
 int storage_fd = -1;
 
 void sigint_cleanup(int _) {
-  (void)_; //to avoid unused var warnings 
-  running = 0;
-  
-  if (storage_fd >= 0) {
-    close(storage_fd);
-  }
+  (void)_; //to avoid unused var warnings
+  node_running = 0;
 }
 
 // add the IP/port into the socks[] map to allow FORWARD finding an
 // already set-up socket, through sock_find()
 // FIXME: bad complexity with many sockets
 int sock_add(in_addr_t inaddr, int port, int sock) {
+  pthread_mutex_lock(&socks_mtx);
   int sock_id = sock_find_addr(inaddr, port);
-  if (sock_id != -1)
+  if (sock_id != -1){
+    pthread_mutex_unlock(&socks_mtx);
     return sock_id;
+  }
   for (int i = 0; i < MAX_SOCKETS; i++) {
     if (socks[i].sock == -1) {
       socks[i].inaddr = inaddr;
       socks[i].port = port;
       socks[i].sock = sock;
+
+      pthread_mutex_unlock(&socks_mtx);
       return i;
     }
   }
+
+  pthread_mutex_unlock(&socks_mtx);
   return -1;
 }
 
 void sock_del_id(int id) {
   assert(id < MAX_SOCKETS);
+
+  pthread_mutex_lock(&socks_mtx);
   cw_log("marking socks[%d] invalid\n", id);
   socks[id].sock = -1;
+  pthread_mutex_unlock(&socks_mtx);
 }
 
 // make entry in socks[] associated to sock invalid, return entry ID if found or -1
 int sock_del(int sock) {
+  pthread_mutex_lock(&socks_mtx);
   int id = sock_find_sock(sock);
-  if (id == -1)
+
+  if (id == -1){
+    pthread_mutex_unlock(&socks_mtx);
     return -1;
+  }
   sock_del_id(id);
+
+  pthread_mutex_unlock(&socks_mtx);
   return id;
 }
 
@@ -158,8 +197,6 @@ size_t safe_recv(int sock, unsigned char *buf, size_t len) {
   return read_tot;
 }
 
-unsigned char fwd_buf[BUF_SIZE];
-
 // Copy m header into m_dst, skipping the first cmd_id elems in m_dst->cmds[]
 void copy_tail(message_t *m, message_t *m_dst, int cmd_id) {
   // copy message header
@@ -177,7 +214,7 @@ void copy_tail(message_t *m, message_t *m_dst, int cmd_id) {
 void forward(int buf_id, message_t *m, int cmd_id) {
   int sock = sock_find_addr(m->cmds[cmd_id].u.fwd.fwd_host, m->cmds[cmd_id].u.fwd.fwd_port);
   assert(sock != -1);
-  message_t *m_dst = (message_t *) fwd_buf;
+  message_t *m_dst = (message_t *) bufs[buf_id].fwd_buf;
   copy_tail(m, m_dst, cmd_id + 1);
   m_dst->req_size = m->cmds[cmd_id].u.fwd.pkt_size;
   cw_log("Forwarding req %u to %s:%d\n", m->req_id,
@@ -186,20 +223,19 @@ void forward(int buf_id, message_t *m, int cmd_id) {
   cw_log("  cmds[] has %d items, pkt_size is %u\n", m_dst->num, m_dst->req_size);
   // TODO: return to epoll loop to handle sending of long packets
   // (here I'm blocking the thread)
-  safe_send(sock, fwd_buf, m_dst->req_size);
+  safe_send(sock, bufs[buf_id].fwd_buf, m_dst->req_size);
 }
 
-unsigned char reply_buf[BUF_SIZE];
+void reply(int sock, int buf_id, message_t *m, int cmd_id) {
+  message_t *m_dst = (message_t *) bufs[buf_id].reply_buf;
 
-void reply(int sock, message_t *m, int cmd_id) {
-  message_t *m_dst = (message_t *) reply_buf;
   copy_tail(m, m_dst, cmd_id + 1);
   m_dst->req_size = m->cmds[cmd_id].u.fwd.pkt_size;
   cw_log("Replying to req %u\n", m->req_id);
   cw_log("  cmds[] has %d items, pkt_size is %u\n", m_dst->num, m_dst->req_size);
   // TODO: return to epoll loop to handle sending of long packets
   // (here I'm blocking the thread)
-  safe_send(sock, reply_buf, m_dst->req_size);
+  safe_send(sock, bufs[buf_id].reply_buf, m_dst->req_size);
 }
 
 size_t recv_message(int sock, unsigned char *buf, size_t len) {
@@ -222,16 +258,15 @@ void compute_for(unsigned long usecs) {
   } while (ts_sub_us(ts_end, ts_beg) < usecs);
 }
 
-unsigned char* store_buf = 0;
 unsigned long blk_size = 0;
 
-ssize_t store(size_t bytes) {
+ssize_t store(int buf_id, size_t bytes) {
   //generate the data to be stored
   if (use_odirect)
     bytes = (bytes + blk_size - 1) / blk_size * blk_size;
   cw_log("STORE: storing %lu bytes\n", bytes);
 
-  safe_write(storage_fd, store_buf, bytes);
+  safe_write(storage_fd, bufs[buf_id].store_buf, bytes);
   fsync(storage_fd);
 
   return bytes;
@@ -248,7 +283,7 @@ ssize_t load(size_t bytes) {
   return read;
 }
 
-int close_and_forget(int sock) {
+int close_and_forget(int epollfd, int sock) {
   cw_log("removing sock=%d from epollfd\n", sock);
   if (epoll_ctl(epollfd, EPOLL_CTL_DEL, sock, NULL) == -1) {
     perror("epoll_ctl: deleting socket");
@@ -259,18 +294,24 @@ int close_and_forget(int sock) {
   return close(sock);
 }
 
-void process_messages(int sock, int buf_id) {
+int process_messages(int sock, int buf_id) {
   size_t received = recv(sock, bufs[buf_id].curr_buf, bufs[buf_id].curr_size, 0);
   cw_log("recv() returned: %d\n", (int)received);
   if (received == 0) {
     cw_log("Connection closed by remote end\n");
-    close_and_forget(sock);
     free(bufs[buf_id].buf);
+    free(bufs[buf_id].reply_buf);
+    free(bufs[buf_id].fwd_buf);
+    free(bufs[buf_id].store_buf);
+    pthread_mutex_lock(&bufs_mtx[buf_id]);
     bufs[buf_id].buf = NULL;
-    return;
+    bufs[buf_id].reply_buf = NULL;
+    pthread_mutex_unlock(&bufs_mtx[buf_id]);
+
+    return 0;
   } else if (received == -1 && (errno == EAGAIN || errno == EWOULDBLOCK)) {
     cw_log("Got EAGAIN or EWOULDBLOCK, ignoring...\n");
-    return;
+    return 1;
   } else if (received == -1) {
     perror("Unexpected error!");
     exit(-1);
@@ -310,11 +351,11 @@ void process_messages(int sock, int buf_id) {
           m->cmds[i].u.fwd.pkt_size += data;
 	  data = -1;
 	}
-	reply(sock, m, i);
+	reply(sock, buf_id, m, i);
 	// any further cmds[] for replied-to hop, not me
 	break;
       } else if (m->cmds[i].cmd == STORE) {
-        store(m->cmds[i].u.store_nbytes);
+        store(buf_id, m->cmds[i].u.store_nbytes);
       } else if (m->cmds[i].cmd == LOAD) {
         data = load(m->cmds[i].u.load_nbytes);
       } else {
@@ -346,6 +387,8 @@ void process_messages(int sock, int buf_id) {
       bufs[buf_id].curr_size = bufs[buf_id].buf_size - leftover;
     }
   }
+
+  return 1;
 }
 
 void send_messages(int buf_id) {
@@ -375,7 +418,7 @@ void *receive_thread(void *data) {
     }
     safe_send(sock, buf, sizeof(message_t));
   }
-  sys_check(close_and_forget(sock));
+  sys_check(close_and_forget(epollfd, sock));
   return 0;
 }
 
@@ -386,10 +429,49 @@ void setnonblocking(int fd) {
    assert(fcntl(fd, F_SETFL, flags) == 0);
 }
 
-#define MAX_EVENTS 10
+void* epoll_worker_loop(void* args) {
+  thread_info* infos = (thread_info*) args;
+  int worker_running = 1;
+
+  while (worker_running) {
+    int nfds = epoll_wait(infos->epollfd, infos->events, MAX_EVENTS, -1);
+    if (nfds == -1) {
+      perror("epoll_wait");
+
+      if (errno == EINTR) {
+        worker_running = 0;
+      } else {
+        exit(EXIT_FAILURE);
+      }
+    }
+
+    for (int i = 0; i < nfds; i++) {
+      int buf_id = infos->events[i].data.u32;
+
+      if ((infos->events[i].events | EPOLLIN) && bufs[buf_id].status == RECEIVING) {
+        worker_running = process_messages(bufs[buf_id].sock, buf_id);
+
+	if (!worker_running) {
+          close_and_forget(infos->epollfd, bufs[buf_id].sock);
+	}
+      } 
+      else if ((infos->events[i].events | EPOLLOUT) && bufs[buf_id].status == SENDING)
+        send_messages(buf_id);
+      else if ((infos->events[i].events | EPOLLOUT) && bufs[buf_id].status == CONNECTING)
+        finalize_conn(buf_id);
+      else {
+        fprintf(stderr, "unexpected status: event=%d, %d\n", infos->events[i].events, bufs[buf_id].status);
+        exit(EXIT_FAILURE);
+      }
+    }
+  }
+
+  return (void*) 1;
+}
+
 void epoll_main_loop(int listen_sock) {
   struct epoll_event ev, events[MAX_EVENTS];
-
+  
   /* Code to set up listening socket, 'listen_sock',
      (socket(), bind(), listen()) omitted */
 
@@ -406,12 +488,17 @@ void epoll_main_loop(int listen_sock) {
     exit(EXIT_FAILURE);
   }
 
-  while (running) {
+  while (node_running) {
     cw_log("epoll_wait()ing...\n");
     int nfds = epoll_wait(epollfd, events, MAX_EVENTS, -1);
     if (nfds == -1) {
-      perror("epoll_wait");
-      exit(EXIT_FAILURE);
+        perror("epoll_wait");
+
+	if (errno == EINTR) {
+          node_running = 0;
+	} else {
+          exit(EXIT_FAILURE);
+	}
     }
 
     for (int i = 0; i < nfds; i++) {
@@ -433,50 +520,51 @@ void epoll_main_loop(int listen_sock) {
         check(orig_sock_id != -1);
 
 	int buf_id;
-	for (buf_id = 0; buf_id < MAX_BUFFERS; buf_id++)
-	  if (bufs[buf_id].buf == 0)
-	    break;
+	for (buf_id = 0; buf_id < MAX_BUFFERS; buf_id++) {
+	  pthread_mutex_lock(&bufs_mtx[buf_id]);
+	  if (bufs[buf_id].buf == 0){
+	    break; //unlock mutex above after mallocs
+	  }
+	  pthread_mutex_unlock(&bufs_mtx[buf_id]);
+	}
 	if (buf_id == MAX_BUFFERS) {
 	  fprintf(stderr, "Not enough buffers for new connection, closing!\n");
-	  close_and_forget(conn_sock);
+	  close_and_forget(epollfd, conn_sock);
 	  continue;
 	}
 	bufs[buf_id].buf = malloc(BUF_SIZE);
-	if (bufs[buf_id].buf == 0) {
+	bufs[buf_id].reply_buf = malloc(BUF_SIZE);
+	bufs[buf_id].fwd_buf = malloc(BUF_SIZE);
+	bufs[buf_id].store_buf = (unsigned char*) aligned_alloc(blk_size, BUF_SIZE + blk_size);
+        
+	assert(bufs[buf_id].store_buf != NULL);
+	if (bufs[buf_id].buf == 0 || bufs[buf_id].reply_buf == 0 || bufs[buf_id].fwd_buf == 0) {
 	  fprintf(stderr, "Not enough memory for allocating new buffer, closing!\n");
-	  close_and_forget(conn_sock);
+	  close_and_forget(epollfd, conn_sock);
 	  continue;
 	}
+        pthread_mutex_unlock(&bufs_mtx[buf_id]);
+
 	bufs[buf_id].buf_size = BUF_SIZE;
 	bufs[buf_id].curr_buf = bufs[buf_id].buf;
 	bufs[buf_id].curr_size = BUF_SIZE;
 	bufs[buf_id].sock = conn_sock;
         bufs[buf_id].status = RECEIVING;
         bufs[buf_id].orig_sock_id = orig_sock_id;
+      
+        ev.events = EPOLLIN | EPOLLOUT;
+        // Use the data.u32 field to store the buf_id in bufs[]
+        ev.data.u32 = buf_id;
+        
+	//prepare client epoll
+        sys_check(thread_infos[buf_id].epollfd = epoll_create1(0));
+        sys_check(epoll_ctl(thread_infos[buf_id].epollfd, EPOLL_CTL_ADD, conn_sock, &ev));
 
-	ev.events = EPOLLIN | EPOLLOUT;
-	// Use the data.u32 field to store the buf_id in bufs[]
-	ev.data.u32 = buf_id;
-	if (epoll_ctl(epollfd, EPOLL_CTL_ADD, conn_sock,
-		      &ev) == -1) {
-	  perror("epoll_ctl: conn_sock");
-	  exit(EXIT_FAILURE);
-	}
-      } else {
-	// FIXME - allow for receiving only message parts, handle EAGAIN
-	int buf_id = events[i].data.u32;
-	cw_log("Receiving and processing on buf_id=%d...\n", buf_id);
-        if ((events[i].events | EPOLLIN) && bufs[buf_id].status == RECEIVING)
-          process_messages(bufs[buf_id].sock, buf_id);
-        else if ((events[i].events | EPOLLOUT) && bufs[buf_id].status == SENDING)
-          send_messages(buf_id);
-        else if ((events[i].events | EPOLLOUT) && bufs[buf_id].status == CONNECTING)
-          finalize_conn(buf_id);
-        else {
-          fprintf(stderr, "unexpected status: event=%d, %d\n", events[i].events, bufs[buf_id].status);
-          exit(EXIT_FAILURE);
-        }
-      }
+        //start client thread
+        pthread_t tid;
+        sys_check(pthread_create(&tid, NULL, epoll_worker_loop, (void*) &thread_infos[buf_id]));
+        sys_check(pthread_detach(tid));
+      } 
     }
   }
 }
@@ -528,6 +616,20 @@ int main(int argc, char *argv[]) {
     socks[i].sock = -1;
   }
 
+  // Init bufs mutexs
+  for (int i = 0; i < MAX_BUFFERS; i++) {
+    sys_check(pthread_mutex_init(&bufs_mtx[i], NULL));
+  }
+
+  // Init socks mutex
+  //TODO: change sock_add and sock_dell's logic to avoid lock re-entrancy
+  pthread_mutexattr_t attr;
+
+  pthread_mutexattr_init(&attr);
+  pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_RECURSIVE); 
+  
+  sys_check(pthread_mutex_init(&socks_mtx, &attr));
+
   // Open storage file, if any
   if (storage_path) {
     int flags = O_RDWR | O_CREAT | O_TRUNC;
@@ -538,8 +640,6 @@ int main(int argc, char *argv[]) {
     sys_check(fstat(storage_fd, &s));
     blk_size = s.st_blksize;
     cw_log("blk_size = %lu\n", blk_size);
-    store_buf = (unsigned char*) aligned_alloc(blk_size, BUF_SIZE + blk_size);
-    assert(store_buf != NULL);
   }
 
   /*---- Create the socket. The three arguments are: ----*/
@@ -569,7 +669,10 @@ int main(int argc, char *argv[]) {
 
   epoll_main_loop(welcomeSocket);
 
-  free(store_buf);
-
+  //termination clean-ups
+  if (storage_fd >= 0) {
+    close(storage_fd);
+  }
+  
   return 0;
 }
