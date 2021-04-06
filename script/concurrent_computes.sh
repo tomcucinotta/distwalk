ef2560f050c45b2852a0cdd5178de3495b8b0b9a
Remo <andreoliremo@gmail.com>
2021-04-06 18:21:46 +0200

    Improved concurrent tests

--- script/concurrent_computes.sh
+++ script/concurrent_computes.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-../src/client -c 1 &
-../src/client -c 1 &
-../src/client -c 1 &
-../src/client -c 1 &
-../src/client -c 1 &
-../src/client -c 1 &
-../src/client -c 1 &
-../src/client -c 1 &
-../src/client -c 1 &
-../src/client -c 1 &
+if [ "$1" = "" ]; then
+    echo "Usage: $0 nclients"
+    exit -1
+fi
+
+../src/node &
+
+for i in $(seq 1 1 $1)
+do
+	../src/client -c 10 &
+done
+
+sleep 2
+
+kill -SIGINT $(pgrep node)
 wait
