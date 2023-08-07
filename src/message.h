f3e9ac0d1ae1b9b518654af66c8d732c51811ed2
Remo <andreoliremo@gmail.com>
2023-08-07 05:18:36 +0200

    message.h struct re-order

--- src/message.h
+++ src/message.h
@@ -10,6 +10,40 @@
 
 typedef enum { COMPUTE, STORE, LOAD, FORWARD, REPLY } command_type_t;
 
+typedef struct {
+  in_addr_t fwd_host;   // target IP of host to forward to
+  uint16_t fwd_port;    // target port (for multiple nodes on same host)
+  uint32_t pkt_size;    // size of forwarded packet
+} fwd_opts_t;
+
+//TODO: consider whether to use this structs
+/*typedef struct {
+  uint64_t offset;
+  uint32_t pkt_size;   // size of forwarded packet
+} store_opts_t;
+
+typedef struct {
+  uint32_t pkt_size;    // size of forwarded packet
+} reply_opts_t;*/
+
+typedef struct {
+  command_type_t cmd;
+  union {
+    uint32_t comp_time_us;  // COMPUTE time (usecs)
+    uint32_t store_nbytes;  // STORE data size
+    uint32_t load_nbytes;   // LOAD data size
+    fwd_opts_t fwd;     // FORWARD host+port and pkt size
+    //reply_opts_t reply;   // REPLY pkt size
+  } u;
+} command_t;
+
+typedef struct {
+  uint32_t req_id;
+  uint32_t req_size;    // Overall message size in bytes, including commands and payload
+  uint8_t num;      // Number of valid entries in cmds[]
+  command_t cmds[]; // Up to 255 command_t
+} message_t;
+
 static inline const char* get_command_name(command_type_t cmd) {
   switch (cmd) {
     case COMPUTE: return "COMPUTE";
@@ -22,39 +56,5 @@ static inline const char* get_command_name(command_type_t cmd) {
       exit(EXIT_FAILURE);
   }
 }
-
-typedef struct {
-  in_addr_t fwd_host;	// target IP of host to forward to
-  uint16_t fwd_port;	// target port (for multiple nodes on same host)
-  uint32_t pkt_size;	// size of forwarded packet
-} fwd_opts_t;
-
-//TODO: consider whether to use this structs
-/*typedef struct {
-  uint64_t offset;
-  uint32_t pkt_size;   // size of forwarded packet
-} store_opts_t;
-
-typedef struct {
-  uint32_t pkt_size;	// size of forwarded packet
-} reply_opts_t;*/
-
-typedef struct {
-  command_type_t cmd;
-  union {
-    uint32_t comp_time_us;	// COMPUTE time (usecs)
-    uint32_t store_nbytes;	// STORE data size
-    uint32_t load_nbytes;	// LOAD data size
-    fwd_opts_t fwd;		// FORWARD host+port and pkt size
-    //reply_opts_t reply;	// REPLY pkt size
-  } u;
-} command_t;
-
-typedef struct {
-  uint32_t req_id;
-  uint32_t req_size;	// Overall message size in bytes, including commands and payload
-  uint8_t num;		// Number of valid entries in cmds[]
-  command_t cmds[];	// Up to 255 command_t
-} message_t;
 
 #endif
