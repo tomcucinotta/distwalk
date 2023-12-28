8e546f7f24af5e684615326e57d475602d6845da
Remo <andreoliremo@gmail.com>
2023-12-28 21:28:02 +0100

    debug: fix edge-case where thread_name is not defined, causing segfault in cw_log()

--- src/cw_debug.h
+++ src/cw_debug.h
@@ -7,13 +7,19 @@
 #include <time.h>
 #include <sys/prctl.h>
 
-#ifdef CW_DEBUG
+// a weak global variable is ignored when a strong one is detected.
+// This is needed for the edge-cases where thread_name is not defined,
+// (e.g. cw_debug is used for unit testing, and not via dw_{client,node})
+__attribute__((weak)) __thread char thread_name[16];
 
-extern __thread char thread_name[16];
+#ifdef CW_DEBUG 
 #define cw_log(msg, ...) do { \
     struct timespec __ts; \
     clock_gettime(CLOCK_MONOTONIC, &__ts);\
-    printf("[%ld.%09ld][%s] " msg, __ts.tv_sec, __ts.tv_nsec, thread_name, ## __VA_ARGS__);  \
+    if (thread_name) \
+      printf("[%ld.%09ld][%s] " msg, __ts.tv_sec, __ts.tv_nsec, thread_name, ## __VA_ARGS__); \
+    else \
+      printf("[%ld.%09ld] " msg, __ts.tv_sec, __ts.tv_nsec, ## __VA_ARGS__); \
     fflush(stdout);  \
   } while (0);
 
