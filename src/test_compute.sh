363c08b896e86368f5930141274bca8f10ef3dc6
Tommaso Cucinotta <tommaso.cucinotta@santannapisa.it>
2024-01-13 10:22:21 +0100

    fixed error handling in script-based tests

--- src/test_compute.sh
+++ src/test_compute.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-trap 'jobs -p | while read pid; do kill $pid; done; wait' SIGINT SIGTERM EXIT
+trap 'jobs -p | while read pid; do kill $pid; done; wait' SIGINT SIGTERM EXIT ERR
+set -e
 
 ./dw_node &
 ./dw_client -C 10000
 elapsed=$(./dw_client -C 10000 | grep elapsed | sed -e 's/.*elapsed: //; s/ us.*//')
-[ $elapsed -gt 10000 ] && [ $elapsed -lt 20000 ] || exit 1
+[ $elapsed -gt 10000 ] && [ $elapsed -lt 20000 ]
 elapsed=$(./dw_client -C 10000 -C 20000 | grep elapsed | sed -e 's/.*elapsed: //; s/ us.*//')
-[ $elapsed -gt 30000 ] && [ $elapsed -lt 40000 ] || exit 1
-
-exit
+[ $elapsed -gt 30000 ] && [ $elapsed -lt 40000 ]
--- src/test_simple.sh
+++ src/test_simple.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-trap 'jobs -p | while read pid; do kill $pid; done; wait' SIGINT SIGTERM EXIT
+trap 'jobs -p | while read pid; do kill $pid; done; wait' SIGINT SIGTERM EXIT ERR
+set -e
 
 ./dw_node &
 ./dw_client
 ./dw_client -n 10
 ./dw_client -C 10000
-
-exit
