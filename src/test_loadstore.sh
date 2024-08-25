e10efd1db5cbea3b3199f1e78ca0657541f0acef
Tommaso Cucinotta <tommaso.cucinotta@santannapisa.it>
2024-08-25 16:31:43 +0200

    adding tests for load/store, in regular and --odirect modes (addressing #30)

---
+++ src/test_loadstore.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+trap 'jobs -p | while read pid; do kill $pid; done; wait' SIGINT SIGTERM EXIT ERR
+set -e
+
+TMP=$(mktemp /tmp/dw-store-XXX.dat)
+
+./dw_node_debug -s $TMP &
+./dw_client_debug -S 65536
+[ $(du -b $TMP | cut -f1) -eq 65536 ]
+
+./dw_client_debug -L 65536
+
+rm $TMP
