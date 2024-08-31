22007cd760020765248af3505a83e0d038652abe
Remo <andreoliremo@gmail.com>
2024-08-31 16:54:55 -0500

    cov: test_compute now covers a use-case that uses fixed bind ports for clients

--- src/test_compute.sh
+++ src/test_compute.sh
@@ -2,9 +2,14 @@
 
 . common.sh
 
-node_bg
-client -C 10000
-elapsed=$(client -C 10000 | grep 'elapsed:' | sed -e 's/.*elapsed: //; s/ us.*//')
+TMP=$(mktemp /tmp/test_compute-XXX.dat)
+
+node_bg &> $TMP
+elapsed=$(client --cl :8000 -C 10000 | grep 'elapsed:' | sed -e 's/.*elapsed: //; s/ us.*//')
 [ $elapsed -gt 10000 ] && [ $elapsed -lt 20000 ]
-elapsed=$(client -C 10000 -C 20000 | grep 'elapsed:' | sed -e 's/.*elapsed: //; s/ us.*//')
+elapsed=$(client --cl :8001 -C 10000 -C 20000 | grep 'elapsed:' | sed -e 's/.*elapsed: //; s/ us.*//')
 [ $elapsed -gt 30000 ] && [ $elapsed -lt 40000 ]
+
+cat $TMP | grep -q "Accepted connection from: 127.0.0.1:8000"
+cat $TMP | grep -q "Accepted connection from: 127.0.0.1:8001"
+rm $TMP
