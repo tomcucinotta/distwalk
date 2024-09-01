72bbea652b578588b6d2129b08a3b2bcdc384ba3
Tommaso Cucinotta <tommaso.cucinotta@santannapisa.it>
2024-09-01 02:03:44 +0200

    test_conn_drop.sh: see how node handles client abruptly terminating

---
+++ src/test_conn_drop.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+. common.sh
+
+node_bg
+
+(client -n 3 -C 100000 -p 1000000 || true) &
+sleep 0.5
+kill -SIGINT $(pidof dw_client_debug)
+kill -SIGINT $(pidof dw_node_debug)
+
+node_bg
+
+(client -n 3 -C 800000 -p 1000000 || true) &
+sleep 0.1
+while ! kill -SIGINT $(pidof dw_client_debug); do
+    echo "Retrying kill of dw_client_debug..."
+done
+
+for ((i=0; i<10; i++)); do
+    (client -n 3 -C 800000 -p 1000000 || true) &
+    sleep 0.$(printf "%06d\n" $[ $RANDOM * 1000000 / 65536 ])
+    while ! kill -SIGINT $(pidof dw_client_debug); do
+          echo "Retrying kill of dw_client_debug..."
+    done
+done
+
+for ((i=0; i<100; i++)); do
+    (client -n 1000 -C 100 -p 1000 --send-pkt-size=$[ 4 * 1024 * 1024 ] || true) &
+    while ! kill -SIGINT $(pidof dw_client_debug); do
+          echo "Retrying kill of dw_client_debug..."
+    done
+done
+
+kill_all SIGKILL
