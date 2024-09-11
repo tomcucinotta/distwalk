ed2ae692cd6554b99fd4b3a4f2a0deee537688e7
Tommaso Cucinotta <tommaso.cucinotta@santannapisa.it>
2024-09-11 02:11:07 +0200

    new test script test_node_affinity.sh tests dw_node --thread-affinity option (related to #30)

---
+++ src/test_node_affinity.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+. common.sh
+
+tmp=$(mktemp /tmp/test_affinity-XXX.dat)
+
+strace_node_bg --thread-affinity=auto &> $tmp
+kill_all SIGINT
+grep "sched_setaffinity(" $tmp
+
+for nt in 2 4; do
+    strace_node_bg --num-threads=$nt --thread-affinity=auto &> $tmp
+    kill_all SIGINT
+    for i in $(seq 0 $[ $nt - 1 ]); do
+        grep "sched_setaffinity(.*\[$i\]" $tmp
+    done
+done
+
+strace_node_bg --thread-affinity=2-2 &> $tmp
+kill_all SIGINT
+grep "sched_setaffinity(.*\[2\]" $tmp
+
+rm $tmp
