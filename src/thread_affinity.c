tomcucinotta/distwalk

commit a14be4e840d0485a8c1fa2ab221b5a727e46d76c

Improved robustness of thread_affinity parser + use 'auto' instead of leaving opt arg empty for automatic, RR thread pinning in dw_node

src/thread_affinity.c
@@ -6,8 +6,10 @@
 #include <pthread.h>
 #include <errno.h>
 #include <assert.h>
+#include <stdlib.h>
 
 #include "thread_affinity.h"
+#include "dw_debug.h"
 
 // Based on taskset human-readable format
 void aff_list_parse(char *str, cpu_set_t* mask, int ncpu) {
@@ -16,9 +18,10 @@ void aff_list_parse(char *str, cpu_set_t* mask, int ncpu) {
     int val;
     int max;
 
+    int step = 1;
     CPU_ZERO(mask);
     while ((tok = strsep(&str, ",")) != NULL) {
-        if (sscanf(tok, "%d-%d", &min, &max) == 2) {
+        if (sscanf(tok, "%d-%d:%d", &min, &max, &step) >= 2) {
             if (min > max) {
                 int tmp = max;
 
@@ -26,18 +29,23 @@ void aff_list_parse(char *str, cpu_set_t* mask, int ncpu) {
                 min = tmp;
             }
             
-            assert(min >= 0 && min < ncpu);
-            assert(max >= 0 && max < ncpu);
+            check(min >= 0 && min < ncpu);
+            check(max >= 0 && max < ncpu);
+            check(step > 0);
 
-            for (int i=min; i<=max; i++) {
+            for (int i=min; i<=max; i += step) {
                 CPU_SET(i, mask);
             }
-        }
-        else if (sscanf(tok, "%d", &val) == 1) {
-            assert(val >= 0 && val < ncpu);
+        } else if (sscanf(tok, "-%d", &max) == 1 || sscanf(tok, "%d-", &min) == 1) {
+            fprintf(stderr, "thread_affinity parsing error: core range '%s' not allowed\n", tok);
+            exit(EXIT_FAILURE);
+        } else if (sscanf(tok, "%d", &val) == 1) {
+            check(val >= 0 && val < ncpu);
             CPU_SET(val, mask);
+        } else {
+            fprintf(stderr, "thread_affinity parsing error: '%s' not allowed\n", tok);
+            exit(EXIT_FAILURE);
         }
-        // else: format error
     }
 }
 

src/dw_node.c
@@ -947,11 +947,10 @@ enum argp_node_option_keys {
     ACCEPT_MODE = 'a',
     STORAGE_OPT_ARG = 's',
     MAX_STORAGE_SIZE = 'm',
-
+    THREAD_AFFINITY = 'c',
     NUM_THREADS = 0x101,
     SYNC,
     ODIRECT,
-    THREAD_AFFINITY,
     NO_DELAY,
     BACKLOG_LENGTH,
 };
@@ -981,7 +980,7 @@ static struct argp_option argp_node_options[] = {
     {"num-threads",       NUM_THREADS,      "n", OPTION_ALIAS },
     {"sync",              SYNC,             "msec",                           0,  "Periodically sync the written data on disk" },
     {"odirect",           ODIRECT,           0,                               0,  "Enable direct disk access"},
-    {"thread-affinity",   THREAD_AFFINITY,  "CORE LIST", OPTION_ARG_OPTIONAL,     "Thread-to-core pinning (optionally specify a list of available cores)"},
+    {"thread-affinity",   THREAD_AFFINITY,  "auto|cX,cZ[,cA-cD[:step]]",            0,   "Thread-to-core pinning (automatic or user-defined list using taskset syntax)"},
     {"no-delay",          NO_DELAY,         "n",                              0, "Set value of TCP_NODELAY socket option [currently not implemented]"},
     {"nd",                NO_DELAY,         "n", OPTION_ALIAS },
     {"help",              HELP,              0,                               0, "Show this help message", 1 },
@@ -1039,7 +1038,7 @@ static error_t argp_node_parse_opt(int key, char *arg, struct argp_state *state)
         break;
     case THREAD_AFFINITY:
         arguments->use_thread_affinity = 1;
-        if (arg && arg[0] != '-') {
+        if (strcmp(arg, "auto") != 0) {
             arguments->thread_affinity_list = arg;
         }
         break;

commit bfe1fba1ca98e0b579107c09f6d32cd34b4706fa

affinity: fixed parsing bug introduced in a14be4e (discovered in be15ed4). Now edge-cases '-c' and 'c-' are reduced to simply 'c' (where c is a core id)

src/thread_affinity.c
@@ -36,11 +36,9 @@ void aff_list_parse(char *str, cpu_set_t* mask, int ncpu) {
             for (int i=min; i<=max; i += step) {
                 CPU_SET(i, mask);
             }
-        } else if (sscanf(tok, "-%d", &max) == 1 || sscanf(tok, "%d-", &min) == 1) {
-            fprintf(stderr, "thread_affinity parsing error: core range '%s' not allowed\n", tok);
-            exit(EXIT_FAILURE);
-        } else if (sscanf(tok, "%d", &val) == 1) {
-            check(val >= 0 && val < ncpu);
+        } else if (sscanf(tok, "%d-", &val) == 1 || sscanf(tok, "-%d", &val) == 1 || sscanf(tok, "%d", &val) == 1) {
+            val = abs(val);
+            check(val < ncpu);
             CPU_SET(val, mask);
         } else {
             fprintf(stderr, "thread_affinity parsing error: '%s' not allowed\n", tok);