0af33df71ccf7afdd239e5b5c3b64dfac37ae1f1
Tommaso Cucinotta <tommaso.cucinotta@santannapisa.it>
2024-09-21 16:49:11 +0200

    test_accept_mode.sh: add sleep 0.5 after strace_node_bg, to wait for both threads to come up and bind/listen, if they have to

--- test/test_accept_mode.sh
+++ test/test_accept_mode.sh
@@ -37,21 +37,24 @@ kill_all SIGINT
 node_bg -a shared --num-threads=2
 [ $(netstat --inet -an | grep -c 7891) -eq 1 ]
 
-tmp=/tmp/dw-log.txt
-
 kill_all SIGINT
+
+tmp=/tmp/dw-log.txt
 
 ## check calls to listen() and bind() with strace
 
 strace_node_bg -a child --num-threads=2 > $tmp 2>&1
+sleep 0.5
 [ $(grep -c listen $tmp) -eq 2 ] && [ $(grep -c bind $tmp) -eq 2 ]
 
 kill_all SIGINT
 
 strace_node_bg -a parent --num-threads=2 > $tmp 2>&1
+sleep 0.5
 [ $(grep -c listen $tmp) -eq 1 ] && [ $(grep -c bind $tmp) -eq 1 ]
 
 kill_all SIGINT
 
 strace_node_bg -a shared --num-threads=2 > $tmp 2>&1
+sleep 0.5
 [ $(grep -c listen $tmp) -eq 1 ] && [ $(grep -c bind $tmp) -eq 1 ]
