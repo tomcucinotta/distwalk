52362bfbdde8abd3bcaaa8cc5e562508cfe1d94d
Tommaso Cucinotta <tommaso.cucinotta@santannapisa.it>
2024-09-21 17:07:04 +0200

    test_stats.sh: use pidof instead of $pid vars, as elsewhere in script

--- test/test_stats.sh
+++ test/test_stats.sh
@@ -43,24 +43,21 @@ kill_all SIGINT
 
 echo "step 3"
 node_bg -b :7891 &> $TMP1
-pid1=$!
 node_bg -b :7892 &> $TMP2
-pid2=$!
 sleep 1
 
 client_bg -C 1000 -F :7892 -C 100 -n 1000
 sleep 2
-kill -SIGUSR1 $pid1
-kill -SIGUSR1 $pid2
+kill -SIGUSR1 `pidof dw_node_debug`
 
 cat $TMP1 | grep -q "worker-id: 0, active-conns: 2"
 cat $TMP2 | grep -q "worker-id: 0, active-conns: 1"
 
 wait `pidof dw_client_debug`
-kill -SIGUSR1 $pid1
-kill -SIGUSR1 $pid2
+kill -SIGUSR1 `pidof dw_node_debug`
+
 cat $TMP1 | grep -q "worker-id: 0, active-conns: 1"
 cat $TMP2 | grep -q "worker-id: 0, active-conns: 1"
 
-#rm $TMP1
-#rm $TMP2
+rm $TMP1
+rm $TMP2
