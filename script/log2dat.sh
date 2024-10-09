67cbdd006349576f80c17959149ce00985a57bb9
Remo Andreoli <andreoliremo@gmail.com>
2024-10-08 23:10:04 -0500

    Updated log2dat.sh with the new dw_client output + added log2csv too

--- script/log2dat.sh
+++ script/log2dat.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 
-grep elapsed $@ | sed -e 's/t: //' -e 's/ us, elapsed://' -e 's/ us//'
+grep elapsed $@ | sed -e 's/t: //' -e 's/ us, elapsed://' -e 's/ us, req_id://' -e 's/, thr_id://' -e 's/, sess_id://' -e 's/ us//'
