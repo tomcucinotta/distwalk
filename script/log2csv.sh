168899d18cc027448d94b04dd6247bc7a07c0b87
Remo Andreoli <andreoliremo@gmail.com>
2024-10-20 07:23:45 +0200

    script: ignore everything before 't: ' in dw_client's log so to enhance the robustness of log2csv.sh

--- script/log2csv.sh
+++ script/log2csv.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 
 echo "t(us),elapsed(us),req_id,thr_id,sess_id"
-grep elapsed $@ | sed -e 's/t: //' -e 's/ us, elapsed: /,/' -e 's/ us, req_id: /,/' -e 's/, thr_id: /,/' -e 's/, sess_id: /,/' -e 's/ us//'
+grep elapsed $@ | sed -e 's/.*t: //' -e 's/ us, elapsed: /,/' -e 's/ us, req_id: /,/' -e 's/, thr_id: /,/' -e 's/, sess_id: /,/' -e 's/ us//'
