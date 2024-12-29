7a5f86b648eaf361b924f62cc08c21a276cac327
Remo Andreoli <andreoliremo@gmail.com>
2024-12-29 22:57:20 +0100

    test: relaxed latency bounds

--- test/test_accept_mode_parent.sh
+++ test/test_accept_mode_parent.sh
@@ -15,19 +15,20 @@ cat $tmp | grep assigned | head -2 | tail -1 | grep -q "assigned to connw-1"
 cat $tmp | grep assigned | head -3 | tail -1 | grep -q "assigned to connw-2"
 
 kill_all SIGINT
+cat $tmp
 rm $tmp
-
 
 node_bg -a parent --nt 2
 
-client -C 1000000 -p 1000000 --nt 4 &> $tmp
+client -C 1s -p 1s --nt 4 &> $tmp
 
 elapsed=($(cat $tmp | grep 'elapsed:' | sed -e 's/.*elapsed: //; s/ us.*//'))
 
-[ ${elapsed[0]} -gt 999000 ] && [ $elapsed -lt 1001000 ]
-[ ${elapsed[1]} -gt 999000 ] && [ $elapsed -lt 1001000 ]
-[ ${elapsed[2]} -gt 1999000 ] && [ $elapsed -lt 2001000 ]
-[ ${elapsed[3]} -gt 1999000 ] && [ $elapsed -lt 2001000 ]
+[ ${elapsed[0]} -gt 990000 ] && [ $elapsed -lt 1010000 ]
+[ ${elapsed[1]} -gt 990000 ] && [ $elapsed -lt 1010000 ]
+[ ${elapsed[2]} -gt 1990000 ] && [ $elapsed -lt 2010000 ]
+[ ${elapsed[3]} -gt 1990000 ] && [ $elapsed -lt 2010000 ]
 
 kill_all SIGINT
+cat $tmp
 rm $tmp
