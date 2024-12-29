855c2a7e725034af7f6603f0659ca1734f0ad7e6
Tommaso Cucinotta <tommaso.cucinotta@santannapisa.it>
2024-12-29 21:41:26 +0100

    test_stats.sh: recovered working (and simplified) test, verifying that after a FORWARD we have a residual connection between the two nodes

--- test/test_stats.sh
+++ test/test_stats.sh
@@ -5,23 +5,26 @@
 TMP1=$(mktemp /tmp/dw-node-stats-1-XXX.dat)
 TMP2=$(mktemp /tmp/dw-node-stats-2-XXX.dat)
 
-echo "step 1"
+
+echo "Step 1"
 node_bg -b :7891 &> $TMP1
 
 kill -SIGUSR1 `pidof dw_node_debug`
+sleep 0.5
 grep -q "worker-id: 0, active-conns: 0, active-reqs: 0" $TMP1
 
 client -C 1000
 
 kill -SIGUSR1 `pidof dw_node_debug`
+sleep 0.5
 cnt=$(grep -c "worker-id: 0, active-conns: 0, active-reqs: 0" $TMP1)
 [ $cnt -eq 2 ]
-
 
 client_bg -C 1000 -n 200
 
 sleep 1
 kill -SIGUSR1 `pidof dw_node_debug`
+sleep 0.5
 grep -q "worker-id: 0, active-conns: 1" $TMP1
 cnt=$(grep -c "worker-id: 0, active-conns: 0, active-reqs: 0" $TMP1)
 [ $cnt -eq 2 ]
@@ -29,7 +32,7 @@ cnt=$(grep -c "worker-id: 0, active-conns: 0, active-reqs: 0" $TMP1)
 kill_all SIGINT
 
 
-echo "step 2"
+echo "Step 2"
 node_bg -b :7891 &> $TMP1
 
 client_bg -C 1000 -n 200
@@ -37,25 +40,25 @@ client_bg -C 1000 -n 200
 sleep 1
 
 kill -SIGUSR1 `pidof dw_node_debug`
+sleep 0.5
 grep -q "worker-id: 0, active-conns: 2, active-reqs: 0" $TMP1
 
 kill_all SIGINT
 wait $(pidof dw_client_debug)
 
-echo "step 3"
+
+echo "Step 3"
 node_bg -b :7891 &> $TMP1
 node_bg -b :7892 &> $TMP2
-sleep 1
 
-client_bg -C 1000 -F :7892 -C 100 -n 1000
-sleep 1
+client -C 10ms -F :7892 -C 20ms
+
 kill -SIGUSR1 `pidof dw_node_debug`
-
-kill -SIGINT `pidof dw_client_debug`
-kill -SIGUSR1 `pidof dw_node_debug`
-
+sleep 0.5
 grep -q "worker-id: 0, active-conns: 1" $TMP1
 grep -q "worker-id: 0, active-conns: 1" $TMP2
+
+kill_all SIGINT
 
 rm $TMP1
 rm $TMP2
