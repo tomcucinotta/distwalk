640b23f6a3e2a9de0b8a1423ce1530e446bca587
Tommaso Cucinotta <tommaso.cucinotta@santannapisa.it>
2025-06-01 10:17:50 +0200

    test_distrib.sh: adding test of gseq: syntax added back in commit cb0bf7bc.

--- test/test_distrib.sh
+++ test/test_distrib.sh
@@ -14,7 +14,7 @@ run test_distrib_debug -d 15M
 run test_distrib_debug -d 15g
 run test_distrib_debug -d 15G
 
-for dist in unif:min=20,max=40 exp:20 exp:20,min=10,max=40 norm:20,std=5 norm:20,std=5,min=10,max=40 lognorm:3,std=1 lognorm:xavg=3,xstd=1 gamma:20,k=1,scale=2 gamma:20,k=2,scale=2 aseq:min=1000,max=2000 aseq:min=1000,max=2000,step=250; do
+for dist in unif:min=20,max=40 exp:20 exp:20,min=10,max=40 norm:20,std=5 norm:20,std=5,min=10,max=40 lognorm:3,std=1 lognorm:xavg=3,xstd=1 gamma:20,k=1,scale=2 gamma:20,k=2,scale=2 aseq:1000,max=2000 aseq:min=1000,max=2000,step=250 gseq:1,max=16 gseq:min=1,max=16,step=2; do
     run test_distrib_debug -d $dist
 done
 
