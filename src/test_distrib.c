b4c9fe0869aa0f64dfb0961c2cc3eda1f2d66fc4
Tommaso Cucinotta <tommaso.cucinotta@santannapisa.it>
2025-08-03 16:44:42 +0200

    test_distrib.c: fixed flaky exit status on success

--- src/test_distrib.c
+++ src/test_distrib.c
@@ -38,4 +38,6 @@ int main(int argc, char **argv) {
     //printf("distrib=%s\n", pd_str(&val));
     for (int i = 0; i < num_samples; i++)
         printf("%g\n", pd_sample(&val));
+
+    return EXIT_SUCCESS;
 }
